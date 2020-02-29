32c564b7da6a3107235f43f9542ec13aa82c9e4c
Asif Rahman <asifrahman@Asifs-MacBook-Pro.local>
2020-02-29 21:32:08 +0530

    Updates

---
+++ precache-manifest.d6d9b5024c1b2ea66ad921ddbdec8cd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39434870a1ab2d6e11e9fb277b2ba65",
+    "url": "/BMI-Calculator/index.html"
+  },
+  {
+    "revision": "60e3e5155aeed1a19af8",
+    "url": "/BMI-Calculator/static/css/main.60ae4819.chunk.css"
+  },
+  {
+    "revision": "05752272744e0a649366",
+    "url": "/BMI-Calculator/static/js/2.20ff69fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/BMI-Calculator/static/js/2.20ff69fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60e3e5155aeed1a19af8",
+    "url": "/BMI-Calculator/static/js/main.e9605ac1.chunk.js"
+  },
+  {
+    "revision": "94a909878adbd68729d5",
+    "url": "/BMI-Calculator/static/js/runtime-main.fe82bec5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BMI-Calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
